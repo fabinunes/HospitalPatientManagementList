5dd09d1c698c9d209881feac30499e54776dcb7a
KLEYTON SOARES <kleysoares@users.noreply.github.com>
2018-11-14 20:08:14 +0000

    Creation of a Node class to be used in the LinkedList.

--- Node.java
+++ Node.java
@@ -1,1 +1,16 @@
+package hospitalpatientmanagementlist;
 
+public class Node<P> {
+    
+    public P patient;
+    public Node<P> next = null;
+    
+    public Node(P patient, Node<P> next) {
+        this.patient = patient;
+        this.next = next;
+    }
+    
+    public Node(P patient) {
+        this(patient, null);
+    }
+}
