b150f576e2c578810f324d9a7a5603cc74fa4ab5
KLEYTON SOARES <kleysoares@users.noreply.github.com>
2018-11-14 20:55:52 +0000

    I started implementing the methods from ControlInterface

--- ControllImplementation.java
+++ ControllImplementation.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hospitalpatientmanagementlist;
 
-/**
- *
- * @author Fabi
- */
-public class ControllImplementation {
+public class ControllImplementation<P> implements ControllInterface<P> {
+
+    public Node<P> head = null;
+    
+    private int size = 0;
+
+    @Override
+    public void getIn(P patient) {
+        if(head == null)
+            getInFirst(patient);
+        else {
+            Node<P> temp = head;
+            while(temp.next != null)
+                temp = temp.next;
+            temp.next = new Node<P>(patient, null);
+        }
+    }
+
+    @Override
+    public void getInFirst(P patient) {
+        head = new Node<P>(patient, head);
+        size++;
+    }
+
+    @Override
+    public void getInAt(int position, P patient) {
+        
+    }
+
+    @Override
+    public P getOut(int pidNumber) {
+        
+    }
+
+    @Override
+    public int seePosition(int pidNumber) {
+    }
+
+    @Override
+    public boolean clearRemainder(int quantity) {
+    }
+
+    @Override
+    public boolean updatePatientInfo(int pidNumber) {
+    }
+
+    @Override
+    public int length() {
+    }
     
 }
